89b45cbc4e1ca60ea745ebd12c9b5ee2769d86fd
spandan saxena <spandansaxena@gmail.com>
2020-11-28 01:06:06 +0530

    Intenting to Login Page from Splash

--- app/src/main/java/com/aaxena/ourjuet/SplashScreen.java
+++ app/src/main/java/com/aaxena/ourjuet/SplashScreen.java
@@ -2,6 +2,7 @@ package com.aaxena.ourjuet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -16,7 +17,8 @@ public class SplashScreen extends AppCompatActivity {
     private void fireSplashScreen() {
         int splash_screen_time_out = 2100;
         new Handler().postDelayed(() -> {
-
+            Intent i=new Intent(SplashScreen.this,Login.class);
+            startActivity(i);
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
             finish();
         }, splash_screen_time_out);
