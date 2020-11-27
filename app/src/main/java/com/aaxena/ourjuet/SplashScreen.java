4fc7545475fa8da87244d704728e85b169c4659a
spandan saxena <spandansaxena@gmail.com>
2020-11-28 01:01:13 +0530

    Added Splash Screen

--- app/src/main/java/com/aaxena/ourjuet/SplashScreen.java
+++ app/src/main/java/com/aaxena/ourjuet/SplashScreen.java
@@ -3,6 +3,7 @@ package com.aaxena.ourjuet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
+        fireSplashScreen();
+    }
+    private void fireSplashScreen() {
+        int splash_screen_time_out = 2100;
+        new Handler().postDelayed(() -> {
+
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            finish();
+        }, splash_screen_time_out);
     }
 }
