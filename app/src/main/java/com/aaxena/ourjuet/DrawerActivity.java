28d8869520fb4b8f86b44881544f529d85e26c93
spandan saxena <spandansaxena@gmail.com>
2020-11-30 19:24:01 +0530

    Added Modules to Drawer Activity

--- app/src/main/java/com/aaxena/ourjuet/DrawerActivity.java
+++ app/src/main/java/com/aaxena/ourjuet/DrawerActivity.java
@@ -1,6 +1,8 @@
 package com.aaxena.ourjuet;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
@@ -10,15 +12,39 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.NavDestination;
 
+import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.navigation.NavigationView;
 
+import javax.inject.Inject;
+
 import dagger.android.AndroidInjector;
+import dagger.android.DispatchingAndroidInjector;
 import dagger.android.support.HasSupportFragmentInjector;
 
 public class DrawerActivity extends AppCompatActivity implements
         NavController.OnDestinationChangedListener
         , NavigationView.OnNavigationItemSelectedListener,
         HasSupportFragmentInjector {
+    @Inject
+    DispatchingAndroidInjector<Fragment> dispatchingAndroidInjector;
+    @Inject
+    SharedPreferencesUtil mPreferencesUtil;
+    public NavigationView mNavigationView;
+    private InterstitialAd mInterstitialAd;
+    private boolean mDoubleBackToExitPressedOnce = false;
+    private AppBarLayout mAppBarLayout;
+    private NavController mNavController;
+    private ActivityDrawerBinding mBinding;
+    AppUpdateManager appUpdateManager;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         return false;
@@ -31,6 +57,15 @@ public class DrawerActivity extends AppCompatActivity implements
 
     @Override
     public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments) {
-        
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setupNavigationComponents();
+        setupAndLoadAds();
+        checkAndShowUpdatesDialog();
+        instantiateFirebase();
     }
 }
