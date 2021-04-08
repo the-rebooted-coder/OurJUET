b95fd2bc33333d50df09b857e56c00206692338b
Spandan Saxena <spandansaxena@gmail.com>
2021-04-08 22:26:20 +0530

    Initialised elements

--- app/src/main/java/com/aaxena/ourjuet/Login.java
+++ app/src/main/java/com/aaxena/ourjuet/Login.java
@@ -1,14 +1,26 @@
 package com.aaxena.ourjuet;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class Login extends AppCompatActivity {
+
+    EditText enr;
+    EditText password;
+    EditText dob;
+    Button signIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        enr = findViewById(R.id.enrNumber);
+        password = findViewById(R.id.password);
+        dob = findViewById(R.id.dob);
+        signIn = findViewById(R.id.loginButton);
     }
 }
